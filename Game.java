e95af6860e98a61cd3a8ecff9bf8dae912a0ac48
qngapparat <jakob.wallnoefer@outlook.com>
2017-06-12 23:55:19 +0200

    finished evaluate() function. Easier to expand now to 5-type-game.

--- Game.java
+++ Game.java
@@ -23,16 +23,26 @@ public class Game {
 
     public void evaluate(Player p1, Player p2) throws Exception {
 
+        //see https://stackoverflow.com/questions/9553058/scalable-solution-for-rock-paper-scissor/9553712#9553712 for more info on the circular principle used here.
+
+        //must be odd in order to work
         if((maxTypes%2) == 0){
             throw new Exception("Number of possibilities must be odd");
         }
 
-        int a = p1.currentChoice;
-        int b = p2.currentChoice;
+        int result = (maxTypes + p1.currentChoice - p2.currentChoice) % maxTypes;
 
-        int result = (maxTypes + a - b) % maxTypes;
+        //it's a draw. Stats will be updated by isDraw
         if(result == 0){
+           return; 
+        }
 
+        else if((result%2) == 1){
+            p1.wins++;
+        }
+
+        else if((result%2) == 0){
+            p2.wins++;
         }
     }
 
@@ -54,7 +64,7 @@ public class Game {
     }
 
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception{
 
         Player p1 = new Player();
 		Player p2 = new Player();
@@ -67,7 +77,13 @@ public class Game {
 
             myGame.printHeader(myGame, p1, p2);
 
-            myGame.evaluate(p1, p2);
+            try{
+                myGame.evaluate(p1, p2);
+            }
+
+            catch(Exception e){
+                throw new Exception(e);
+            }
             myGame.isDraw(myGame, p1, p2);
 			myGame.updateGameStats(myGame, p1, p2);
 
