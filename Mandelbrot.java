73241d97e32d69a7eec62eb631f59220d184a8a0
qngapparat <jakob.wallnoefer@outlook.com>
2017-06-12 20:15:37 +0200

    first working built

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,28 +1,27 @@
 public class Mandelbrot {
 
     public static void main(String[] args) {
-        for (double yCoordinate = -1.2; yCoordinate < 1.2; yCoordinate += 0.05) {
-            for (double xCoordinate = -2.2; xCoordinate < 1.0; xCoordinate += 0.025) {
+        double deltaY = 0.05;
+        for (double yCoordinate = -1.2; yCoordinate < 1.2; yCoordinate += deltaY) {
+            double deltaX = 0.025;
+            for (double xCoordinate = -2.2; xCoordinate < 1.0; xCoordinate += deltaX) {
                 double a = 0.0;
                 double b = 0.0;
-                int j;
                 int maxIterations = 100;
-                double infinityTreshold = 2;
-                double g = 0.0;
-                for (j = 0; j < maxIterations; j++) {
-                    double zr = a * a;
-                    double zi = 2 * a * b; //using infinityTreshold as 2 doesn't make sense here
-                    zr -= b * b;
-                    zr += xCoordinate;
-                    zi += yCoordinate;
-                    a = zr;
-                    b = zi;
-                    g = Math.sqrt(a * a + b * b);
-                    if (g > infinityTreshold) {
+                double infinityThreshold = 2;
+                double offset = 0.0;
+                for (int j = 0; j < maxIterations; j++) {
+                    double realPart = (a * a) - (b * b) + xCoordinate;
+                    double imaginaryPart = 2 * a * b + yCoordinate;
+
+                    a = realPart;
+                    b = imaginaryPart;
+                    offset = Math.sqrt(a * a + b * b);
+                    if (offset > infinityThreshold) {
                         break;
                     }
                 }
-                if (g > infinityTreshold) {
+                if (offset > infinityThreshold) {
                     System.out.print(" ");
                 } else {
                     System.out.print("\u25a0");
